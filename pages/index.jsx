8391ac16d5819fc5f154f1fc0992a0c8d81c7b7a
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-14 10:52:34 +0200

    🛠 fix vercel deploy errors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useCallback } from "react";
 import { connect } from "react-redux";
 import PlaylistsRow from "../components/PlaylistsRow";
 import {
@@ -34,7 +34,7 @@ export default connect(
   getNewReleases,
   countryCode,
 }) {
-  useLayoutEffect(() => {
+  const RenderPlaylists = useCallback(() => {
     (async () => {
       if (!recentlyPlaylists?.items?.length)
         await getRecentlyPlayedLists(token);
@@ -45,6 +45,10 @@ export default connect(
       if (!newReleasePlaylists?.items?.length)
         await getNewReleases(token, countryCode);
     })();
+  }, []);
+
+  useLayoutEffect(() => {
+    RenderPlaylists();
   }, []);
   return (
     <div className="app_home_page_content_area">
