88cbcac6690ec02d564cad5ee363bcf00025ced3
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-14 11:34:14 +0200

    fix missed fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  optimizeFonts: false,
   images: {
     domains: ["i.scdn.co", "seed-mix-image.spotifycdn.com", "*"],
   },
