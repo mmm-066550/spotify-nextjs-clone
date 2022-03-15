95b690a6a66602fffc8ca1153d8abc6a7e2f3020
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-16 01:04:20 +0200

    16/3/2022 1:05 AM

--- components/AppMain/index.jsx
+++ components/AppMain/index.jsx
@@ -10,89 +10,102 @@ import { getBrowseCategories } from "../../redux/actions";
 import { connect } from "react-redux";
 import { useLayoutEffect } from "react";
 
-export default connect(state => state, { getBrowseCategories })(function AppMain({ token, countryCode, children, getBrowseCategories }) {
-  const [scrollBtn, setscrollBtn] = useState(false);
-  const [open, setOpen] = useState(false);
-  const router = useRouter();
-  const container = useRef(null);
-  const [categoriesPerRender, _] = useState(1);
-  const [offset, setOffset] = useState(0);
+export default connect((state) => state, { getBrowseCategories })(
+  function AppMain({ token, countryCode, children, getBrowseCategories }) {
+    const [scrollBtn, setscrollBtn] = useState(false);
+    const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const container = useRef(null);
+    const [categoriesPerRender, _] = useState(1);
+    const [offset, setOffset] = useState(5);
 
-  useEffect(() => {
-    container.current.scrollTop = 0;
-  }, [router]);
+    useEffect(() => {
+      container.current.scrollTop = 0;
+    }, [router]);
 
-  useLayoutEffect(() => {
-    setOpen(JSON.parse(window.localStorage.getItem("isAsideOpen")) || false);
-  }, []);
+    useLayoutEffect(() => {
+      setOpen(JSON.parse(window.localStorage.getItem("isAsideOpen")) || false);
+    }, []);
 
-  if (typeof window !== "undefined") {
-    window.onresize = () => {
-      if (window.innerWidth < 991) {
-        if (open) setOpen(false);
-      } else {
-        if (!open) setOpen(true);
-      }
-    };
-    if (container?.current)
-      container?.current?.onscroll = () => {
-        if (router.pathname === '/') {
-          if ((container?.current?.scrollHeight) - (container?.current?.offsetHeight) === container?.current?.scrollTop) {
-            loadMoreCategories()
-          }
+    if (typeof window !== "undefined") {
+      window.onresize = () => {
+        if (window.innerWidth < 991) {
+          if (open) setOpen(false);
+        } else {
+          if (!open) setOpen(true);
         }
-        if (container?.current?.scrollTop >= 400) setscrollBtn(true);
-        if (container?.current?.scrollTop < 100) setscrollBtn(false);
-      }
-  }
+      };
+      if (container.current)
+        container.current.onscroll = () => {
+          if (router.pathname === "/") {
+            if (
+              container?.current?.scrollHeight -
+                container?.current?.offsetHeight ===
+              container?.current?.scrollTop
+            ) {
+              loadMoreCategories();
+            }
+          }
+          if (container?.current?.scrollTop >= 400) setscrollBtn(true);
+          if (container?.current?.scrollTop < 100) setscrollBtn(false);
+        };
+    }
 
-  const loadMoreCategories = () => {
-    setOffset(offset + 1);
-    getBrowseCategories(
-      token,
-      countryCode,
-      categoriesPerRender,
-      categoriesPerRender * (offset + 1)
-    );
-  }
+    const loadMoreCategories = () => {
+      setOffset(offset + 1);
+      getBrowseCategories(
+        token,
+        countryCode,
+        categoriesPerRender,
+        categoriesPerRender * (offset + 1)
+      );
+    };
 
-  return (
-    <main
-      className={`${styles.app_main_area} ${open ? styles.aside_open : ""}`}
-    >
-      <div className={styles.app_main_top_section}>
-        <AppSidebar
-          open={open}
-          setOpen={setOpen}
-          style={styles.app_main_sidebar}
-        />
-        <div ref={container} className={`${styles.app_main_func_container}`}>
-          <div className="position-absolute w-100">
-            <ActionsTopBar />
-            <div className={styles.container}>
-              <>
-                {scrollBtn ? (
-                  <button
-                    title="Scroll To The Top"
-                    className={styles.scroll_top_btn}
-                    onClick={() => (container.current.scrollTop = 0)}
-                  >
-                    <FiChevronUp />
-                  </button>
-                ) : null}
-                {children}
-                {router.pathname === '/' && categoriesPerRender * (offset + 1) <= 40 ? <div className={styles.loading_more_spinner}><div className={`spinner-grow ${styles.grow_spinner}`} role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div></div> : null}
-              </>
+    return (
+      <main
+        className={`${styles.app_main_area} ${open ? styles.aside_open : ""}`}
+      >
+        <div className={styles.app_main_top_section}>
+          <AppSidebar
+            open={open}
+            setOpen={setOpen}
+            style={styles.app_main_sidebar}
+          />
+          <div ref={container} className={`${styles.app_main_func_container}`}>
+            <div className="position-absolute w-100">
+              <ActionsTopBar />
+              <div className={styles.container}>
+                <>
+                  {scrollBtn ? (
+                    <button
+                      title="Scroll To The Top"
+                      className={styles.scroll_top_btn}
+                      onClick={() => (container.current.scrollTop = 0)}
+                    >
+                      <FiChevronUp />
+                    </button>
+                  ) : null}
+                  {children}
+                  {router.pathname === "/" &&
+                  categoriesPerRender * (offset + 1) <= 40 ? (
+                    <div className={styles.loading_more_spinner}>
+                      <div
+                        className={`spinner-grow ${styles.grow_spinner}`}
+                        role="status"
+                      >
+                        <span className="visually-hidden">Loading...</span>
+                      </div>
+                    </div>
+                  ) : null}
+                </>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <footer className={styles.app_main_bottom_section}>
-        <SignupBanner />
-      </footer>
-    </main>
-  );
-}
-)
+        <footer className={styles.app_main_bottom_section}>
+          <SignupBanner />
+        </footer>
+      </main>
+    );
+  }
+);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect } from "react";
 import { connect } from "react-redux";
 import PlaylistsRow from "../components/PlaylistsRow";
 import {
@@ -38,17 +38,13 @@ export default connect(
   getBrowseCategories,
   browseCategories,
 }) {
-  const [categoriesPerRender, _] = useState(1);
-  const [offset, setOffset] = useState(0);
   useLayoutEffect(() => {
     if (!recentlyPlaylists?.items?.length) getRecentlyPlayedLists(token);
     if (!featuredPlaylists?.items?.length) getFeaturedList(token, countryCode);
     if (!artistsPlaylists?.items?.length) getTopLikedArtists(token);
     if (!albumsPlaylists?.items?.length) getTopLikedTracks(token);
     if (!newReleasePlaylists?.items?.length) getNewReleases(token, countryCode);
-    if (!browseCategories.length)
-      getBrowseCategories(token, countryCode, categoriesPerRender, offset);
-    // (token, country, catsLimit, offset)
+    if (!browseCategories.length) getBrowseCategories(token, countryCode, 5, 0);
   }, []);
   return (
     <div className="app_home_page_content_area">
@@ -80,23 +76,7 @@ export default connect(
       {browseCategories.map((category, i) => {
         return <PlaylistsRow key={i} content={category} placeholder={5} />;
       })}
-      <div className="d-flex text-center">
-        {/* {categoriesPerRender * (offset + 1) <= 40 ? (
-          <button
-            onClick={() => {
-              setOffset(offset + 1);
-              getBrowseCategories(
-                token,
-                countryCode,
-                categoriesPerRender,
-                categoriesPerRender * (offset + 1)
-              );
-            }}
-          >
-            load more
-          </button>
-        ) : null} */}
-      </div>
+      <div className="d-flex text-center"></div>
     </div>
   );
 });
