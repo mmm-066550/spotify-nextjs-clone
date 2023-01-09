4874d3426005da38a2a4082e4f776d5f50f026df
Moustapha Mahmoud <mmm066550@gmail.com>
2023-01-09 18:45:59 +0200

    Update spotifyLogin.js

--- utils/spotifyLogin.js
+++ utils/spotifyLogin.js
@@ -3,7 +3,7 @@ export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 export const REDIRECT_URI =
   typeof window !== "undefined"
     ? window.location.origin
-    : "https://spotify-mmm066550.vercel.app";
+    : "https://spotify-mmm066550.netlify.app";
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 export const RESPONSE_TYPE = "code";
